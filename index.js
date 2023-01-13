907a431246161b8e705d5e5443f8098bfaaaab96
dimitarkolev <dk@almero.bg>
2023-01-13 16:50:08 +0000

    Fix

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 import { onProcess, useLogger, useOperations, constants, mikser } from 'mikser-core'
 import _ from 'lodash'
 import morphism from 'morphism'
+import jsonata from 'jsonata'
 
 onProcess(() => {
     const logger = useLogger()
 
-    for (let { match, schema, map } of mikser.config.mapper?.mappers || []) {        
+    for (let { match, schema, map, expression, use = 'meta' } of mikser.config.mapper?.mappers || []) {        
         const entities = useOperations([constants.OPERATION_CREATE, constants.OPERATION_UPDATE])
         .map(operation => operation.entity)
         .filter(entity => entity.meta && _.matches(entity, match))
+
+        if (expression) {
+            expression = jsonata(expression)
+        }
     
         for (let entity of entities) {
             logger.trace('Mapper: %s', entity.id)
+            const object = _.get(entity, use)
             if (schema) {
-                entity.meta = morphism.morphism(schema, entity.meta)
+                _.set(entity, use, morphism.morphism(schema, object))
+            }
+            if (expression) {
+                _.set(entity, use, expression(object))
             }
             if (map) {
-                entity.meta = map(entity.meta)
+                _.set(entity, use, map(object))
             }
         }
     }
